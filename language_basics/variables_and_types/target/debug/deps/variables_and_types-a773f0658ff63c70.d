226042f69e0dbc18d060b63807b8e0df3e62cb57
ashiq72 <ahmedashik18k@gmail.com>
2024-11-17 11:03:10 +0600

    Fix nested

---
+++ language_basics/variables_and_types/target/debug/deps/variables_and_types-a773f0658ff63c70.d
@@ -0,0 +1,5 @@
+/Users/dev4/Desktop/rust/language_basics/variables_and_types/target/debug/deps/variables_and_types-a773f0658ff63c70: src/main.rs
+
+/Users/dev4/Desktop/rust/language_basics/variables_and_types/target/debug/deps/variables_and_types-a773f0658ff63c70.d: src/main.rs
+
+src/main.rs:
---
+++ language_basics/variables_and_types/target/debug/variables_and_types.d
@@ -0,0 +1,1 @@
+/Users/dev4/Desktop/rust/language_basics/variables_and_types/target/debug/variables_and_types: /Users/dev4/Desktop/rust/language_basics/variables_and_types/src/main.rs
